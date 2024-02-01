937a9ddb438ff67357d21b39a86bf40c4cf066aa
AssahBismarkabah <134822946+AssahBismarkabah@users.noreply.github.com>
2024-02-01 15:56:39 +0000

    Create IntSquare.java

---
+++ IntSquare.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        IntSquare intSquare = new IntSquare(6);
+
+        int square = intSquare.getSquare(5);
+        System.out.println("Square: " + square);
+    }
+}
