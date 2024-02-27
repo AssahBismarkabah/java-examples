ecd7dcdf0e23f28560a41b83c0b3c1a5db0b7447
AssahBismarkabah <assah9799@gmail.com>
2024-02-27 17:52:45 +0100

    feat: added my spring work

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        IntSquare intSquare = new IntSquare(6);
-
-        int square = intSquare.getSquare(5);
-        System.out.println("Square: " + square);
-    }
-}
